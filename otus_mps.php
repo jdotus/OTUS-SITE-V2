8ee5f11b73ebad0b4750dfa26dc9d58ce974ceb4
John David S. Cabal <Johndavidcabal000@gmail.com>
2025-10-10 09:08:46 +0800

    Removing unused pictures

--- otus_mps.php
+++ otus_mps.php
@@ -9,7 +9,15 @@
     </div>
 
     <div class="main-container">
-        
+        <h1 class="test-1">Why Choose OTUS Managed Print Services</h1>
+        <hr class="test-1 award-sliders-divider">
+        <div class="intro">
+            <div class="intro-content test-1">
+                <p>At <span>Otus Copy System Inc.</span>, our <span>Managed Print Services</span> simplify fleet management with one predictable monthly plan that covers <span>toner, repairs, and proactive maintenance</span>  so your team can focus on work.</p>
+            </div>
+            
+            <div class="intro-image test-1"></div>
+        </div>
     </div>
 
 <?php include('scripts.php'); ?>
