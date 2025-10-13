9b5ef9b8992d245496157d86416bb3e0c159f0d1
John David S. Cabal <Johndavidcabal000@gmail.com>
2025-10-13 08:48:33 +0800

    done

--- otus_mps.php
+++ otus_mps.php
@@ -16,7 +16,20 @@
                 <p>At <span>Otus Copy System Inc.</span>, our <span>Managed Print Services</span> simplify fleet management with one predictable monthly plan that covers <span>toner, repairs, and proactive maintenance</span>  so your team can focus on work.</p>
             </div>
             
-            <div class="intro-image test-1"></div>
+            <div class="intro-image test-1">
+                <img height="550" width="550" src="../OTUS_SITE_V2.1/img/Our_Services/Untitled-1.png" alt="SERVICES IMAGE">
+            </div>
+        </div>
+
+        <div class="comprehensive-support">
+            <h1 class="test-1">Comprehensive Technical Support: Your Local Experts, Always On Call</h1>
+            <hr class="test-1 award-sliders-divider">
+            <p class="test-1">Even the most advanced technology needs expert care. We pride ourselves on offering fast, local, and utterly reliable technical support. When a machine needs attention, you need a solution now—not next week. That's the Otus promise.</p>
+
+            <div class="comprehensive-main-content">
+
+            </div>
+
         </div>
     </div>
 
